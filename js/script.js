3e660570fe83e1b0b225337d96ab2c45648c7de5
miasriko <info@neuf.design>
2025-04-09 10:30:09 +0900

    コード作成

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const css = `
+  <style>
+    body {
+      background-attachment: fixed;
+      background-repeat: no-repeat;
+      background-position: center top;
+      background-image: url("hoge");
+      background-size: 1920px 1080px;
+    }
+  </style>
+`;
+document.write(css);
