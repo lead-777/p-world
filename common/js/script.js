6b050e64de92502b38150a944f7d1d062fe0a4ca
miasriko <info@neuf.design>
2025-04-10 12:00:38 +0900

    更新

--- common/js/script.js
+++ common/js/script.js
@@ -4,7 +4,6 @@ const css = `
       background-attachment: fixed;
       background-repeat: no-repeat;
       background-position: top center;
-      background-image: url('https://miasriko.github.io/p-world/osaka/s-cosmo-city-acty/img/bg.gif');
       background-size: cover;
     }
   </style>
