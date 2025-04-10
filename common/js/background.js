903d7f7e79a2eb00cb340607e22300780f7167e9
miasriko <info@neuf.design>
2025-04-10 12:37:54 +0900

    ディレクトリ変更

--- common/js/background.js
+++ common/js/background.js
@@ -1,10 +1,16 @@
 const background = `
   <style>
-    body {
+    .background {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
       background-attachment: fixed;
       background-repeat: no-repeat;
       background-position: top center;
       background-size: contain;
+      z-index: -1;
     }
   </style>
 `;
